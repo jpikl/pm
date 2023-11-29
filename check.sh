0d83d575f7d5220c40cfb58c618c566c03cd3440
Jan Pikl <pikl.jan@gmail.com>
2023-11-30 00:47:15 +0100

    Add install hints to check.sh

--- check.sh
+++ check.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
-shfmt -w pm
-shellcheck pm
+run() {
+    if [ -x "$(command -v "$1")" ]; then
+        "$@"
+    else
+        echo >&2 "$1 not found, install it using 'pm install $1'"
+        exit 1
+    fi
+}
 
+run shfmt -w pm
+run shellcheck pm
