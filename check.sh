42f41dd9d21663adc62664694ddd17c4d3311587
Jan Pikl <pikl.jan@gmail.com>
2023-12-31 00:25:25 +0100

    Simplify check.sh

--- check.sh
+++ check.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env sh
 
-run() {
-    if [ -x "$(command -v "$1")" ]; then
-        "$@"
-    else
-        echo >&2 "$1 not found, install it using 'pm install $1'"
-        exit 1
-    fi
-}
-
-run shfmt -w pm
-run shellcheck pm
+shfmt -w pm
+shellcheck pm
